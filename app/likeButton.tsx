f7aecbec6cc81c4aee4c5e1cfdf688a7c62725d8
Dan Edwards <igneosaur@gmail.com>
2023-10-09 08:52:39 +0100

    Add like count to likeButton

--- app/likeButton.tsx
+++ app/likeButton.tsx
@@ -2,15 +2,24 @@
 
 import { useState } from "react";
 
+type Like = {
+  id: string;
+  created_at: Date;
+  user_id: string;
+  gif_id: string;
+};
+
 export type LikeButtonProps = {
   gifID: string;
-  isLiked?: boolean;
+  likes: Like[];
 };
 
 export default function LikeButton({
   gifID,
-  isLiked = false,
+  likes: initialLikes,
 }: LikeButtonProps) {
+  const [likes, setLikes] = useState(initialLikes);
+  const [isLiked, setIsLiked] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
   async function handleClick() {
@@ -19,8 +28,14 @@ export default function LikeButton({
       const response = await fetch(`/api/likes/${gifID}`, {
         method: "POST",
       });
-      const json = await response.json();
-      console.log({ json });
+      const { isAdded, data } = await response.json();
+      if (isAdded) {
+        setLikes([...likes, data]);
+        setIsLiked(true);
+      } else {
+        setLikes(likes.filter(({ id }) => id !== data.id));
+        setIsLiked(false);
+      }
     } catch (error) {
       console.error({ error });
     } finally {
@@ -35,7 +50,8 @@ export default function LikeButton({
       className="bg-black p-2 hover:bg-gray-900 disabled:opacity-50"
       title="Like"
     >
-      {isLiked ? "🤍" : "♡"}
+      {isLiked ? "🤍 " : "♡ "}
+      {likes.length}
     </button>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import { getTrendingData } from "@/lib/giphy";
 import Image from "next/image";
 import LikeButton from "./likeButton";
+import prisma from "@/lib/prisma";
 
 export default async function Home() {
-  const data = await getTrendingData({ limit: 10, rating: "g" });
+  const gifs = await getTrendingData({ limit: 10, rating: "g" });
+  const allLikes = await prisma.likes.findMany();
   return (
     <main className="main bg-zinc-800/30 h-screen overflow-auto snap-mandatory snap-y">
-      {data.map(({ id, images, title }) => {
+      {gifs.map(({ id, images, title }) => {
         const gif_id = String(id);
+        const likes = allLikes.filter((like) => like.gif_id === gif_id);
         return (
           <section
             id={gif_id}
@@ -22,7 +25,7 @@ export default async function Home() {
               height={512}
               priority
             />
-            <LikeButton gifID={gif_id} />
+            <LikeButton gifID={gif_id} likes={likes} />
           </section>
         );
       })}
